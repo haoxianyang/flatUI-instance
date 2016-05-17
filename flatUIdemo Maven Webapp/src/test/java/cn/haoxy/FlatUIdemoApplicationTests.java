2d396412879bd2144fd77b4c0af76a748bd333e8
haoxy <xianyang.hao@vivame.cn>
2016-05-17 16:46:48 +0800

    framework test class.

---
+++ flatUIdemo Maven Webapp/src/test/java/cn/haoxy/FlatUIdemoApplicationTests.java
@@ -0,0 +1,18 @@
+package cn.haoxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cn.haoxy.boot.SampleController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SampleController.class)
+public class FlatUIdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
